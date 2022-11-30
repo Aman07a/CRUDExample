298df7cc2a0b8a1db5932c4767f2dbe3fb700c13
Aman Ahmed <aman16.aa17@gmail.com>
2022-11-30 08:57:03 +0100

    Project Overview

--- CRUDExample/Program.cs
+++ CRUDExample/Program.cs
@@ -1,6 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if(builder.Environment.IsDevelopment())
+{
+	app.UseDeveloperExceptionPage();
+}
+
+app.UseStaticFiles();
+app.UseRouting();
+app.MapControllers();
 
 app.Run();
