5f7ba6183dd58c6aed114e5b699780aa4fd221f5
Aman Ahmed <aman16.aa17@gmail.com>
2022-11-30 15:37:46 +0100

    Add Country - xUnit Test - Part 3

--- CRUDTests/CountriesServiceTest.cs
+++ CRUDTests/CountriesServiceTest.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using Entities;
 using ServiceContracts.DTO;
 using ServiceContracts;
+using Services;
 
 namespace CRUDTests
 {
@@ -12,6 +13,68 @@ namespace CRUDTests
 
 		public CountriesServiceTest()
 		{
+			_countriesService = new CountriesService();
+		}
+
+		// When CountryAddRequest is null, it should throw ArgumentException
+		[Fact]
+		public void AddCountry_NullCountry()
+		{
+			// Arrange
+			CountryAddRequest? request = null;
+
+			// Assert
+			Assert.Throws<ArgumentNullException>(() =>
+			{
+				// Act
+				_countriesService.AddCountry(request);
+			});
+		}
+
+		// When the CountryName is null, it should throw ArgumentException
+		[Fact]
+		public void AddCountry_CountryNameIsNull()
+		{
+			// Arrange
+			CountryAddRequest? request = new CountryAddRequest() { CountryName = null };
+
+			// Assert
+			Assert.Throws<ArgumentException>(() =>
+			{
+				// Act
+				_countriesService.AddCountry(request);
+			});
+		}
+
+		// When the CountryName is duplicate, it should throw ArgumentException
+		[Fact]
+		public void AddCountry_DuplicateCountryName()
+		{
+			// Arrange
+			CountryAddRequest? request1 = new CountryAddRequest() { CountryName = "USA" };
+			CountryAddRequest? request2 = new CountryAddRequest() { CountryName = "USA" };
+
+			// Assert
+			Assert.Throws<ArgumentException>(() =>
+			{
+				// Act
+				_countriesService.AddCountry(request1);
+				_countriesService.AddCountry(request2);
+			});
+		}
+
+		// When you supply proper country name, it should insert (add) the country to the existing list of countries
+		[Fact]
+		public void AddCountry_ProperCountryDetails()
+		{
+			// Arrange
+			CountryAddRequest? request = new CountryAddRequest() { CountryName = "Japan" };
+
+			// Assert
+			CountryResponse response = _countriesService.AddCountry(request);
+
+			// Act
+			Assert.True(response.CountryID != Guid.Empty);
 		}
 	}
 }
