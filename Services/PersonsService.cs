faa0122ba1e1b22f8617ca3d28bad1887a98c697
Aman Ahmed <aman16.aa17@gmail.com>
2022-12-01 15:33:40 +0100

    Get Person By Person ID - Implementation

--- Services/PersonsService.cs
+++ Services/PersonsService.cs
@@ -58,7 +58,15 @@ namespace Services
 
 		public PersonResponse? GetPersonByPersonID(Guid? personID)
 		{
-			throw new NotImplementedException();
+			if (personID == null)
+				return null;
+
+			Person? person = _persons.FirstOrDefault(temp => temp.PersonID == personID);
+
+			if (person == null)
+				return null;
+
+			return person.ToPersonResponse();
 		}
 	}
 }
