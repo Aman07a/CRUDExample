50375dccaaab438783b5ffd913d4f3ed5409af48
Aman Ahmed <aman16.aa17@gmail.com>
2022-12-09 11:45:02 +0100

    List View

--- CRUDExample/Controllers/PersonsController.cs
+++ CRUDExample/Controllers/PersonsController.cs
@@ -1,14 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ServiceContracts;
+using ServiceContracts.DTO;
 
 namespace CRUDExample.Controllers
 {
 	public class PersonsController : Controller
 	{
+		// Private fields
+		private readonly IPersonsService _personsService;
+
+		// Constructor
+		public PersonsController(IPersonsService personsService)
+		{
+			_personsService = personsService;
+		}
+
 		[Route("persons/index")]
 		[Route("/")]
 		public IActionResult Index()
 		{
-			return View();
+			List<PersonResponse> persons = _personsService.GetAllPersons();
+
+			// Views/Persons/Index.cshtml
+			return View(persons);
 		}
 	}
 }
