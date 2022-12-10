e88a89a8245a7c14d673247f6a8e5e8f32108886
Aman Ahmed <aman16.aa17@gmail.com>
2022-12-10 18:46:18 +0100

    Seed Data

--- Entities/Person.cs
+++ Entities/Person.cs
@@ -15,17 +15,20 @@ namespace Entities
 		[StringLength(40)]
 		public string? PersonName { get; set; }
 
+        // Nvarchar(40)
         [StringLength(40)]
         public string? Email { get; set; }
 
 		public DateTime? DateOfBirth { get; set; }
 
+        // Nvarchar(10)
         [StringLength(10)]
         public string? Gender { get; set; }
 
 		// UniqueIdentifier
 		public Guid? CountryID { get; set; }
 
+        // Nvarchar(200)
         [StringLength(200)]
         public string? Address { get; set; }
 
