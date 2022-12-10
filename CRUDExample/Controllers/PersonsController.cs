98a7fa52213a465dccd227a773d9912b7a2f8834
Aman Ahmed <aman16.aa17@gmail.com>
2022-12-10 15:52:20 +0100

    Edit View

--- CRUDExample/Controllers/PersonsController.cs
+++ CRUDExample/Controllers/PersonsController.cs
@@ -6,90 +6,137 @@ using ServiceContracts.Enums;
 
 namespace CRUDExample.Controllers
 {
-    [Route("[controller]")]
-    public class PersonsController : Controller
-    {
-        // Private fields
-        private readonly IPersonsService _personsService;
-        private readonly ICountriesService _countriesService;
+	[Route("[controller]")]
+	public class PersonsController : Controller
+	{
+		// Private fields
+		private readonly IPersonsService _personsService;
+		private readonly ICountriesService _countriesService;
 
-        // Constructor
-        public PersonsController(IPersonsService personsService, ICountriesService countriesService)
-        {
-            _personsService = personsService;
-            _countriesService = countriesService;
-        }
+		// Constructor
+		public PersonsController(IPersonsService personsService, ICountriesService countriesService)
+		{
+			_personsService = personsService;
+			_countriesService = countriesService;
+		}
 
-        // Url: index
-        [Route("[action]")]
-        [Route("/")]
-        public IActionResult Index(string searchBy, string? searchString, string sortBy = nameof(PersonResponse.PersonName), SortOrderOptions sortOrder = SortOrderOptions.ASC)
-        {
-            // Search
-            ViewBag.SearchFields = new Dictionary<string, string>()
-            {
-                { nameof(PersonResponse.PersonName), "Person Name" },
-                { nameof(PersonResponse.Email), "Email" },
-                { nameof(PersonResponse.DateOfBirth), "Date of Birth" },
-                { nameof(PersonResponse.Gender), "Gender" },
-                { nameof(PersonResponse.CountryID), "Country" },
-                { nameof(PersonResponse.Address), "Address" },
-            };
+		// Url: index
+		[Route("[action]")]
+		[Route("/")]
+		public IActionResult Index(string searchBy, string? searchString, string sortBy = nameof(PersonResponse.PersonName), SortOrderOptions sortOrder = SortOrderOptions.ASC)
+		{
+			// Search
+			ViewBag.SearchFields = new Dictionary<string, string>()
+			{
+				{ nameof(PersonResponse.PersonName), "Person Name" },
+				{ nameof(PersonResponse.Email), "Email" },
+				{ nameof(PersonResponse.DateOfBirth), "Date of Birth" },
+				{ nameof(PersonResponse.Gender), "Gender" },
+				{ nameof(PersonResponse.CountryID), "Country" },
+				{ nameof(PersonResponse.Address), "Address" },
+			};
 
-            List<PersonResponse> persons =
-                _personsService.GetFilteredPersons(searchBy, searchString);
+			List<PersonResponse> persons =
+				_personsService.GetFilteredPersons(searchBy, searchString);
 
-            ViewBag.CurrentSearchBy = searchBy;
-            ViewBag.SearchString = searchString;
+			ViewBag.CurrentSearchBy = searchBy;
+			ViewBag.SearchString = searchString;
 
-            // Sort
-            List<PersonResponse> sortedPersons =
-                _personsService.GetSortedPersons(persons, sortBy, sortOrder);
+			// Sort
+			List<PersonResponse> sortedPersons =
+				_personsService.GetSortedPersons(persons, sortBy, sortOrder);
 
-            ViewBag.CurrentSortBy = sortBy;
-            ViewBag.CurrentSortOrder = sortOrder.ToString();
+			ViewBag.CurrentSortBy = sortBy;
+			ViewBag.CurrentSortOrder = sortOrder.ToString();
 
-            // Views/Persons/Index.cshtml
-            return View(sortedPersons);
-        }
+			// Views/Persons/Index.cshtml
+			return View(sortedPersons);
+		}
 
-        // Executes when the user clicks on "Create Person" hyperlink (while opening the create view)
-        // Url: persons/create
-        [Route("[action]")]
-        [HttpGet]
-        public IActionResult Create()
-        {
-            List<CountryResponse> countries = _countriesService.GetAllCountries();
-            ViewBag.Countries = countries.Select(temp =>
-                new SelectListItem() { Text = temp.CountryName, Value = temp.CountryID.ToString() }
-            );
+		// Executes when the user clicks on "Create Person" hyperlink (while opening the create view)
+		// Url: persons/create
+		[Route("[action]")]
+		[HttpGet]
+		public IActionResult Create()
+		{
+			List<CountryResponse> countries = _countriesService.GetAllCountries();
+			ViewBag.Countries = countries.Select(temp =>
+				new SelectListItem() { Text = temp.CountryName, Value = temp.CountryID.ToString() }
+			);
 
-            // new SelectListItem() { Text="Harsha", Value="1" }
-            // <option value="1">Harsha</option>
-            return View();
-        }
+			// new SelectListItem() { Text="Harsha", Value="1" }
+			// <option value="1">Harsha</option>
+			return View();
+		}
 
-        [HttpPost]
-        // Url: persons/create
-        [Route("[action]")]
-        public IActionResult Create(PersonAddRequest personAddRequest)
-        {
-            if (!ModelState.IsValid)
-            {
-                List<CountryResponse> countries = _countriesService.GetAllCountries();
-                ViewBag.Countries = countries.Select(temp =>
-                    new SelectListItem() { Text = temp.CountryName, Value = temp.CountryID.ToString() }
-                );
+		[HttpPost]
+		// Url: persons/create
+		[Route("[action]")]
+		public IActionResult Create(PersonAddRequest personAddRequest)
+		{
+			if (!ModelState.IsValid)
+			{
+				List<CountryResponse> countries = _countriesService.GetAllCountries();
+				ViewBag.Countries = countries.Select(temp =>
+					new SelectListItem() { Text = temp.CountryName, Value = temp.CountryID.ToString() }
+				);
 
-                ViewBag.Errors = ModelState.Values.SelectMany(v => v.Errors).Select(e => e.ErrorMessage).ToList();
-                return View();
-            }
+				ViewBag.Errors = ModelState.Values.SelectMany(v => v.Errors).Select(e => e.ErrorMessage).ToList();
+				return View();
+			}
 
-            // Call the service method
-            PersonResponse personResponse = _personsService.AddPerson(personAddRequest);
+			// Call the service method
+			PersonResponse personResponse = _personsService.AddPerson(personAddRequest);
 
-            // Navigate to Index() action method (it makes another get request to "persons/index")
-            return RedirectToAction("Index", "Persons");
-        }
-    }
+			// Navigate to Index() action method (it makes another get request to "persons/index")
+			return RedirectToAction("Index", "Persons");
+		}
+
+		[HttpGet]
+		//Eg: /persons/edit/1
+		[Route("[action]/{personID}")]
+		public IActionResult Edit(Guid personID)
+		{
+			PersonResponse? personResponse = _personsService.GetPersonByPersonID(personID);
+			if (personResponse == null)
+			{
+				return RedirectToAction("Index");
+			}
+
+			PersonUpdateRequest personUpdateRequest = personResponse.ToPersonUpdateRequest();
+
+			List<CountryResponse> countries = _countriesService.GetAllCountries();
+			ViewBag.Countries = countries.Select(temp =>
+				new SelectListItem() { Text = temp.CountryName, Value = temp.CountryID.ToString() });
+
+			return View(personUpdateRequest);
+		}
+
+		[HttpPost]
+		[Route("[action]/{personID}")]
+		public IActionResult Edit(PersonUpdateRequest personUpdateRequest)
+		{
+			PersonResponse? personResponse = _personsService.GetPersonByPersonID(personUpdateRequest.PersonID);
+
+			if (personResponse == null)
+			{
+				return RedirectToAction("Index");
+			}
+
+			if (ModelState.IsValid)
+			{
+				PersonResponse updatedPerson = _personsService.UpdatePerson(personUpdateRequest);
+				return RedirectToAction("Index");
+			}
+			else
+			{
+				List<CountryResponse> countries = _countriesService.GetAllCountries();
+				ViewBag.Countries = countries.Select(temp =>
+					new SelectListItem() { Text = temp.CountryName, Value = temp.CountryID.ToString() });
+
+				ViewBag.Errors = ModelState.Values.SelectMany(v => v.Errors).Select(e => e.ErrorMessage).ToList();
+				return View();
+			}
+		}
+	}
 }
