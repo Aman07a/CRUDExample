5a76e354130cd987a596111722ad2534ab48d2c0
Aman Ahmed <aman16.aa17@gmail.com>
2022-12-10 18:17:50 +0100

    Connection String

--- CRUDExample/Program.cs
+++ CRUDExample/Program.cs
@@ -2,6 +2,8 @@ using ServiceContracts;
 using Services;
 using Microsoft.EntityFrameworkCore;
 using Entities;
+using Microsoft.Data.SqlClient;
+using System;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
@@ -12,7 +14,7 @@ builder.Services.AddSingleton<IPersonsService, PersonsService>();
 
 builder.Services.AddDbContext<PersonsDbContext>(options =>
 {
-    options.UseSqlServer();
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
 var app = builder.Build();
