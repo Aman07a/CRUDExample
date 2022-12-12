e6c66819aa92ef60886bd1dd1b93d8b5508e5b93
Aman Ahmed <aman16.aa17@gmail.com>
2022-12-12 08:44:35 +0100

    Excel to Database Upload - Part 1

--- ServiceContracts/ICountriesService.cs
+++ ServiceContracts/ICountriesService.cs
@@ -1,5 +1,6 @@
 ﻿using ServiceContracts.DTO;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 
 namespace ServiceContracts
 {
@@ -27,5 +28,12 @@ namespace ServiceContracts
 		/// <param name="countryID">CountryID (guid) to search</param>
 		/// <returns>Matching country as CountryResponse object</returns>
 		Task<CountryResponse?> GetCountryByCountryID(Guid? countryID);
+
+		/// <summary>
+		/// Uploads countries from excel file into database
+		/// </summary>
+		/// <param name="formFile">Excel file with list of countries</param>
+		/// <returns>Returns number of countries added</returns>
+		Task<int> UploadCountriesFromExcelFile(IFormFile formFile);
 	}
 }
